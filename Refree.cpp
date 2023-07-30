8f254ec444d3c0c8673db08d11244735f12ef040
a1810246 <a1810246@student.adelaide.edu.au>
2023-07-30 14:42:20 +0000

    Jul 30, 2023, 14:42:20 UTC

--- Refree.cpp
+++ Refree.cpp
@@ -2,6 +2,20 @@
 #include<string>
 #include "Refree.h"
 
-Refree::Refree{
-    
+Refree::Refree(){
+
+}
+
+Player* Refree::refGame(Player* player1, Player* player2){
+    char move_human = player1->makeMove();
+    char move_computer = player2->makeMove();
+    if(move_human == move_computer){
+        std::cout<<"It's a Tie."<<std::endl;
+    }
+    if(move_human == 'S'){
+        std::cout<<player2->getName() <<"Wins!"<<std::endl;
+    }
+    if(move_human == 'P'){
+        std::cout<<player1->getName()
+    }
 }

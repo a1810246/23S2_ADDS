9af3f58a9b93df462ac1ae7d68acd2d0183bbdda
a1810246 <a1810246@student.adelaide.edu.au>
2023-07-31 02:59:41 +0000

    Jul 31, 2023, 02:59:41 UTC

--- Referee.cpp
+++ Referee.cpp
@@ -10,16 +10,16 @@ Player* Referee::refGame(Player* player1, Player* player2){
     char move_human = player1->makeMove();
     char move_computer = player2->makeMove();
     if(move_human == move_computer){
-        std::cout<<"Tie"<<std::endl;
+        //std::cout<<"Tie"<<std::endl;
         return nullptr;
     }
     if(move_human == 'P'){
-        std::cout<<player2->getName() <<std::endl;
+        //std::cout<<player2->getName() <<std::endl;
         return player1;
     }
     if(move_human == 'S'){
-        std::cout<<player1->getName() <<std::endl;
+        //std::cout<<player1->getName() <<std::endl;
         return player2;
     }
-    Player * winner = 
+    Player * winner =
 }

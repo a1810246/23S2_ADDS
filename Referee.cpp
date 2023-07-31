e266adcc12fc15f713d6563ec06f29eb0e6f4f10
a1810246 <a1810246@student.adelaide.edu.au>
2023-07-31 01:05:09 +0000

    submission

---
+++ Referee.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include "Referee.h"
+
+Referee::Referee(){
+
+}
+
+Player* Referee::refGame(Player* player1, Player* player2){
+    char move_human = player1->makeMove();
+    char move_computer = player2->makeMove();
+    if(move_human == move_computer){
+        std::cout<<"It's a Tie."<<std::endl;
+        return nullptr;
+    }
+    if(move_human == 'S'){
+        std::cout<<player2->getName() <<"Wins."<<std::endl;
+        return player2;
+    }
+    if(move_human == 'P'){
+        std::cout<<player1->getName() <<"Wins."<<std::endl;
+        return player1;
+    }
+}
---
+++ Referee.h
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+#pragma once
+#include"Player.h"
+
+class Referee{
+    public:
+    Referee();
+    Player* refGame(Player* player1, Player* player2);
+};

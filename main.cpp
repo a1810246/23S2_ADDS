d7fd36dd7519ef05e87573b80c5e974ec8639763
a1810246 <a1810246@student.adelaide.edu.au>
2023-07-31 00:51:24 +0000

    Jul 31, 2023, 00:51:24 UTC

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include"Human.h"
+#include"Player.h"
+#include"Refree.h"
+#include"Computer.h"
+using namespace std;
+
+int main(){
+    Human test1;
+    Computer test2;
+
+    cout<<test1.getName()<<endl;
+
+    return 0;
+}

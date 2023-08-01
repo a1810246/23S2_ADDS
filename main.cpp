852d239fca7cf10a1d40b766a4c1a326939f62d0
a1810246 <a1810246@student.adelaide.edu.au>
2023-08-01 10:01:16 +0000

    Aug 1, 2023, 10:01:16 UTC

--- main.cpp
+++ main.cpp
@@ -2,21 +2,34 @@
 #include<string>
 #include"Human.h"
 #include"Player.h"
+#include<iostream>
+#include<string>
+#include"Human.h"
+#include"Player.h"
 #include"Referee.h"
 #include"Computer.h"
 using namespace std;
 
 int main(){
-    Player* test1 = new Human();
-    Player* test2 = new Computer();
-    Referee ref;
 
-    //cout<<test2.getName()<<endl;
-    Player * result = ref.refGame(test1, test2);
-    if (result == nullptr) {
-        std::cout << "Tie" << std::endl;
-    } else {
-        std::cout << result->getName()<< std::endl;
+
+    Player* test1 =nullptr;
+    Player* test2 =nullptr;
+    Referee ref;
+    Player * result=nullptr;
+    int i;
+
+    //test2 = new Computer();
+    for (i=0;i<4;i++){
+
+        test1 = new Human();
+        test2 = new Computer();
+        result = ref.refGame(test1, test2);
+        if (result == nullptr) {
+            std::cout << "Tie" << std::endl;
+        } else {
+            std::cout << result->getName()<< std::endl;
+        }
     }
 
     return 0;
